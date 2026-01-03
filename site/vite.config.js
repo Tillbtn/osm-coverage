765f01463ab3ed32d98a2e0fbbc7f5ef14d41e7e
Till Butenschön <t.butenschoen@web.de>
2026-01-03 23:09:25 +0100

    fix vite config

--- site/vite.config.js
+++ site/vite.config.js
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
     build: {
         rollupOptions: {
             input: {
-                main: resolve(__dirname, 'index.html'),
-                addresses: resolve(__dirname, 'addresses.html'),
-                buildings: resolve(__dirname, 'buildings.html'),
-                nds: resolve(__dirname, 'nds.html'),
-                nrw: resolve(__dirname, 'nrw.html'),
-                rlp: resolve(__dirname, 'rlp.html'),
+                main: 'addresses.html',
             },
         },
     },
